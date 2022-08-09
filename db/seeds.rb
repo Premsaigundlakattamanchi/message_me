c970cb8197bc13af3a9dfa3e4812d1bb4cb3d0c6
Premsai <premsaicsk@gmail.com>
2022-08-09 19:20:02 +0530

    p

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+User.create(username:"Gowtham",password:"password")
+User.create(username:"santhosh",password:"password")
+User.create(username:"rajesh",password:"password")
+User.create(username:"malli",password:"password")
+User.create(username:"ramesh",password:"password")
